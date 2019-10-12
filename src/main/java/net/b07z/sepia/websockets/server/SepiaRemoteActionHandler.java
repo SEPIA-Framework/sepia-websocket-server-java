cfbf4218fcd09eec78e5a9ed50a79f0be45fa352
Florian Quirin <info@bytemind.de>
2019-10-12 22:58:38 +0200

    fixed and improved remote-action handler

--- src/main/java/net/b07z/sepia/websockets/server/SepiaRemoteActionHandler.java
+++ src/main/java/net/b07z/sepia/websockets/server/SepiaRemoteActionHandler.java
@@ -1,17 +1,21 @@
 package net.b07z.sepia.websockets.server;
 
+import java.util.List;
+
 import org.eclipse.jetty.websocket.api.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.b07z.sepia.server.core.tools.JSON;
-import net.b07z.sepia.websockets.common.SocketConfig;
 import net.b07z.sepia.websockets.common.SocketMessage;
+import net.b07z.sepia.websockets.common.SocketUser;
+import net.b07z.sepia.websockets.common.SocketUserPool;
 import net.b07z.sepia.websockets.common.SocketMessage.DataType;
-import net.b07z.sepia.websockets.common.SocketMessage.TextType;
 
 /**
- * Handle remote key requests sent to server form a SEPIA client.
+ * Handle remote action requests sent to server REST API form a SEPIA client.<br>
+ * <br>
+ * NOTE: Currently remote action requests are only allowed when sent via REST API 
+ * endpoint and then converted to an assistant socket message. 
  * 
  * @author Florian Quirin
  */
@@ -30,30 +34,70 @@ public class SepiaRemoteActionHandler implements ServerMessageHandler {
 
 	@Override
 	public void handle(Session userSession, SocketMessage msg) throws Exception {
-		String remoteUserId = (String) msg.data.get("user");
-		String targetDeviceId = (String) msg.data.get("targetDeviceId"); 	//TODO: can be "<auto>"
+		//NOTE: userSession is the session of the assistant NOT the user since it is the mediator between the REST API and socket message
+		//Currently remote actions can only be sent to the user who triggered them
+		SocketUser user = findRemoteTargetSocketUser(msg);
+		if (user == null){
+			//log.info("SepiaRemoteActionHandler: Could not find target user. Message will not be sent."); 		//debug
+			return;
+		}
 		String remoteMsgType = (String) msg.data.get("type");
 		String action = (String) msg.data.get("action");
-		//channelId, deviceId ...
-		//TODO: we really should add deviceId here to redirect request to a specific device and only fallback to active user if ID not given
 		
-		//rebuild message to stay clean (?)
-		String receiver = remoteUserId;		//currently remote actions can only be sent to the user who triggered them
-		
-		String channelId = msg.channelId; 		//NOTE: this can be modified by parent message handler, e.g. <auto> -> specific channel
-		SocketMessage remoteMsg = new SocketMessage(channelId, 
-				SocketConfig.SERVERNAME, SocketConfig.localName, 		//TODO: change this to msg.sender, msg.senderDeviceId ?? Or do we need this for auth. ?? 
-				receiver, targetDeviceId,
-				(remoteUserId + " sent remoteAction: " + remoteMsgType), TextType.status.name()		//TODO: replace with SepiaSocketBroadcaster.makeServerStatusMessag() ??
+		//build new message - we send this as server message
+		String receiver = user.getUserId();		//the user who triggered the action via REST API from any device and receives it via socket message on target client
+		String statusText = (receiver + " sent remoteAction: " + remoteMsgType); 
+		SocketMessage remoteMsg = SepiaSocketBroadcaster.makeServerStatusMessage(null, 
+				user.getActiveChannel(), 
+				statusText, 
+				DataType.remoteAction, false
 		);
-		remoteMsg.data = JSON.make("user", remoteUserId, "type", remoteMsgType, "action", action,
-				"dataType", DataType.remoteAction.name());
-		//remoteMsg.setUserList(getUserList());
-		if (msg.msgId != null) remoteMsg.setMessageId(msg.msgId);
+		remoteMsg.addData("user", receiver);
+		remoteMsg.addData("type", remoteMsgType);
+		remoteMsg.addData("action", action);
 		
-		//TODO: is this actually a single session broadcast?
-		//preBroadcastAction(user, remoteMsg, true, false, true);
-        server.broadcastMessage(remoteMsg, userSession);
+		//broadcast to target user session
+        server.broadcastMessage(remoteMsg, user.getUserSession());
+	}
+	
+	//--------------------
+	
+	/**
+	 * Find the targeted user for the remote action by checking the message data.<br>
+	 * Note: If target device ID and/or channel ID are set to 'auto' the first result that fits both is taken.
+	 * @return user or null
+	 */
+	public static SocketUser findRemoteTargetSocketUser(SocketMessage msg){
+		//the actual user information is in the data
+		String remoteUserId = (String) msg.data.get("remoteUserId");
+		String targetDeviceId = (String) msg.data.get("targetDeviceId");
+		String targetChannelId = (String) msg.data.get("targetChannelId");
+		boolean channelIdIsAuto = (targetChannelId == null || targetChannelId.isEmpty() || targetChannelId.equals("<auto>"));
+		boolean targetDeviceIsAuto = (targetDeviceId == null || targetDeviceId.isEmpty() || targetDeviceId.equals("<auto>"));
+		
+		SocketUser user = null; //SocketUserPool.getActiveUserById(remoteUserId);
+		if (remoteUserId != null && !remoteUserId.isEmpty()){
+			//find the right session
+			List<SocketUser> possibleRemoteUsers = SocketUserPool.getAllUsersById(remoteUserId);
+			boolean correctDevice = false;
+			boolean correctChannel = false;
+			for (SocketUser su : possibleRemoteUsers){
+				correctDevice = (targetDeviceIsAuto || (targetDeviceId.equals(su.getDeviceId())));
+				correctChannel = (channelIdIsAuto || (targetChannelId.equals(su.getActiveChannel())));
+				if (correctDevice && correctChannel){
+					user = su;
+				}
+			}
+			if (user != null){
+				//debug:
+				/*
+				System.out.println("RemoteUserId: " + user.getUserId());
+				System.out.println("TargetChannel: " + user.getActiveChannel());
+				System.out.println("TargetDevice: " + user.getDeviceId());
+				*/
+			}
+		}
+		return user;
 	}
 
 }
